176ab8b3e37ed0972a5dd54fe503dee003dfde68
Asad Zia <asaad_zia@hotmail.com>
2017-03-31 23:31:49 +0100

    Add files via upload
    v1.0

---
+++ TestActivities/TestActivities/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Activities;
+using System.Text;
+using System.ComponentModel;
+
+
+namespace TestActivities
+{
+    public class CustomActivity : CodeActivity
+    {
+        [Category("Input")]
+        public InArgument<double> FirstNumber { get; set; }
+
+        [Category("Input")]
+        public InArgument<double> SecondNumber { get; set; }
+
+        [Category("Output")]
+        public OutArgument<double> Result { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var firstNumber = FirstNumber.Get(context);
+            var secondNumber = SecondNumber.Get(context);
+            var result = secondNumber + firstNumber;
+            Result.Set(context, result);
+        }
+    }
+}
